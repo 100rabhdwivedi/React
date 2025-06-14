41f950b267025d4a6565ce0620b87d1cfd1ab531
100rabhdwivedi <dwivedi100rabhh@gmail.com>
2025-06-14 09:02:30 +0530

    use of useState

--- map-usestate/src/App.jsx
+++ map-usestate/src/App.jsx
@@ -1,38 +1,42 @@
+import { useState } from "react";
 
 const App = () => {
-
   let data = [
     {
-      name:"saurabh",
-      age:18,
+      name: "saurabh",
+      age: 18,
     },
     {
-      name:"sivam",
-      age : 15,
+      name: "sivam",
+      age: 15,
     },
     {
-      name:"surya",
-      age:15
-    }
-  ]
+      name: "surya",
+      age: 15,
+    },
+  ];
 
-  let updatedData = data.map((elem,index)=>{
+  const [userLocation, setUserLocation] = useState("Mumbai");
+
+  let updatedData = data.map((elem, index) => {
     return (
       <li key={index}>
-        <span>Name: {elem.name}</span> |
-        <small>Age: {elem.age}</small>
+        <span>Name: {elem.name}</span> |<small>Age: {elem.age}</small>
       </li>
-    )
-    
-  })
+    );
+  });
+
+  let handleClick = () =>{
+    setUserLocation("Banglore")
+  }
   return (
     <div>
       <h1>Rendering Json data</h1>
-      <ul>
-        {updatedData}
-      </ul>
+      <ul>{updatedData}</ul>
+      <ul>{userLocation}</ul>
+      <button onClick={handleClick}>change Loc</button>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
